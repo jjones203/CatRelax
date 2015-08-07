f04ff9fb8e06728939b48f0796f09726a8aa20ad
JESSICA JONES <JJONES203@UNM.EDU>
2015-08-07 16:51:20 -0600

    updated with PawPoints

--- app/src/main/java/test/jessicajones/interfacecomponent/VideoMenu.java
+++ app/src/main/java/test/jessicajones/interfacecomponent/VideoMenu.java
@@ -1,7 +1,7 @@
 package test.jessicajones.interfacecomponent;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,48 +11,25 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
+/**
+ * VideoMenu displays list of video titles user can select. If an item is selected,
+ * VideoPlay is launched and the title is passed to VideoPlay as an intent.
+ * @author jessica jones
+ */
 public class VideoMenu extends ListActivity {
 
     public final static String TITLE = "test.jessicajones.interfacecomponent.TITLE";
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
 
-
+    /* Create menu; set up listener */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_video_menu);
-
-
-        String[] videoArray = getResources().getStringArray(R.array.video_array);
-
-        // Define a new adapter
-        ArrayAdapter<String> mAdapter;
-        mAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, videoArray);
-
-        setListAdapter(mAdapter);
-
-
-
-        // Define the listener interface
-        AdapterView.OnItemClickListener mListener = new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                Intent intent = new Intent(getApplicationContext(),VideoPlay.class);
-                String title = (String) (((TextView) view).getText());
-                intent.putExtra(TITLE,title);
-                startActivity(intent);
-            }
-        };
-
-        // Get the ListView and wired the listener
-        ListView listView = getListView();
-        listView.setOnItemClickListener(mListener);
+        setTitle("CatRelax Video Menu");
+        setUpList();
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,16 +40,64 @@ public class VideoMenu extends ListActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        // changing default in attempt to solve Galaxy-related error
         if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(getApplicationContext(), VideoMenu.class);
+            startActivity(settingsIntent);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Overriding onResume so number of PawPoints refreshed when user returns to menu
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        setUpList();
+    }
+
+    /**
+     * Helper method for creating list of menu items; called by onCreate and onResume
+      */
+    private void setUpList()
+    {
+        // Get video titles from strings.xml
+        String[] videoArray = getResources().getStringArray(R.array.video_array);
+        int numVideos = videoArray.length;
+        String[] menuTextArray = new String[numVideos];
+        for (int i = 0; i < numVideos; i++) {
+            String videoName = videoArray[i];
+            // Retrieve number PawPoints via SharedPreferences
+            SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+            int points = prefs.getInt(videoName, 0); //0 is the default value.
+            menuTextArray[i] = videoName + " - " + points +" PawPoints";
+        }
+
+        // Define a new adapter
+        ArrayAdapter rowAdapter;
+        rowAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, menuTextArray);
+        setListAdapter(rowAdapter);
+
+        // Define the listener interface
+        AdapterView.OnItemClickListener menuListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Intent intent = new Intent(getApplicationContext(),VideoPlay.class);
+                String title =  (((TextView) view).getText()).toString().split(" ")[0];
+                // Pass title of video selected to VideoPlay with intent
+                intent.putExtra(TITLE,title);
+                startActivity(intent);
+            }
+        };
+
+        // Get the ListView and wire the listener
+        ListView listView = getListView();
+        listView.setOnItemClickListener(menuListener);
     }
 }
--- app/src/main/java/test/jessicajones/interfacecomponent/VideoPlay.java
+++ app/src/main/java/test/jessicajones/interfacecomponent/VideoPlay.java
@@ -2,60 +2,75 @@ package test.jessicajones.interfacecomponent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.VideoView;
 import android.widget.MediaController;
 
-public class VideoPlay extends Activity {
+/**
+ * VideoPlay creates a VideoView with the video file corresponding to the
+ * video tile passed in the intent.The video displays on full screen and
+ * loops indefinitely.
+ */
+public class VideoPlay extends Activity implements MediaPlayer.OnPreparedListener {
+    String videoName;
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
 
+    /* Set up and start video */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // set flags for fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_video_play);
 
+        // get the video file, use it to set VideoView
         Intent intent = getIntent();
-        String title = intent.getStringExtra(VideoMenu.TITLE);
-        TextView titleText = (TextView) findViewById(R.id.playVideoTitle);
-        titleText.setText(title);
-
         VideoView playVideoView = (VideoView)findViewById(R.id.playVideoView);
-
-        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sample_mpeg4);
+        videoName = intent.getStringExtra(VideoMenu.TITLE).toLowerCase();
+        int id = getResources().getIdentifier(videoName, "raw", getPackageName());
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + id);
         playVideoView.setVideoURI(uri);
 
+        // finish setting up VideoView, start playing
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(playVideoView);
         playVideoView.setMediaController(mediaController);
-
+        playVideoView.setOnPreparedListener(this);
         playVideoView.requestFocus();
         playVideoView.start();
+
     }
 
-
+    //implement looping
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_video_play, menu);
+    public void onPrepared(MediaPlayer mp) {
+        mp.setLooping(true);
+    }
+
+    /**
+     * When user touches screen, increments PawPoints for that title
+     * (stored in SharedPreferences).
+     * @param event
+     * @return
+     */
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String videoTitle = getIntent().getStringExtra(VideoMenu.TITLE);
+        int count = prefs.getInt(videoTitle, 0); //0 is the default value.
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(videoTitle, ++count);
+        editor.commit();
+
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
